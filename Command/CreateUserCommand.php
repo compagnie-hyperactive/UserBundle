fec970dafecfe9e59074077fcda68dba5f744dbc
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-02-16 18:29:51 +0100

    Change command namespace

--- Command/CreateUserCommand.php
+++ Command/CreateUserCommand.php
@@ -36,7 +36,7 @@ class CreateUserCommand extends Command {
 	protected function configure() {
 		$this
 			// the name of the command (the part after "bin/console")
-			->setName( 'app:user:create' )
+			->setName( 'lch:user:create' )
 			// the short description shown while running "php bin/console list"
 			->setDescription( 'Creates a new user.' )
 			// the full command description shown when running the command with
